694a34396cedbefb16520179f96ccab863f61bfd
William Kennedy <bill@ardanlabs.com>
2023-12-11 15:50:29 -0500

    commit 11

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -79,6 +79,18 @@ func run(ctx context.Context, log *logger.Logger) error {
 	}
 
 	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
+
+	// -------------------------------------------------------------------------
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
