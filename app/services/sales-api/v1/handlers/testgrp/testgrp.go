dc9770e99a4caf18b3f2647d6525eb75683caf62
William Kennedy <bill@ardanlabs.com>
2023-12-12 14:49:16 -0500

    commit 21

--- app/services/sales-api/v1/handlers/testgrp/testgrp.go
+++ app/services/sales-api/v1/handlers/testgrp/testgrp.go
@@ -2,12 +2,18 @@ package testgrp
 
 import (
 	"context"
+	"errors"
+	"math/rand"
 	"net/http"
 
 	"github.com/ardanlabs/service/foundation/web"
 )
 
 func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		return errors.New("NON-TRUSTING ERROR")
+	}
+
 	status := struct {
 		Status string
 	}{
---
+++ business/web/v1/mid/errors.go
@@ -0,0 +1,54 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+
+	v1 "github.com/ardanlabs/service/business/web/v1"
+	"github.com/ardanlabs/service/foundation/logger"
+	"github.com/ardanlabs/service/foundation/web"
+)
+
+func Errors(log *logger.Logger) web.Middleware {
+	m := func(handler web.Handler) web.Handler {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			if err := handler(ctx, w, r); err != nil {
+				log.Error(ctx, "message", "msg", err)
+
+				var er v1.ErrorResponse
+				var status int
+
+				switch {
+				case v1.IsTrustedError(err):
+					trsErr := v1.GetTrustedError(err)
+					er = v1.ErrorResponse{
+						Error: trsErr.Error(),
+					}
+					status = trsErr.Status
+
+				default:
+					er = v1.ErrorResponse{
+						Error: http.StatusText(http.StatusInternalServerError),
+					}
+					status = http.StatusInternalServerError
+				}
+
+				if err := web.Respond(ctx, w, er, status); err != nil {
+					return err
+				}
+
+				// If we receive the shutdown err we need to return it
+				// back to the base handler to shut down the service.
+				if web.IsShutdown(err) {
+					return err
+				}
+			}
+
+			return nil
+		}
+
+		return h
+	}
+
+	return m
+}
