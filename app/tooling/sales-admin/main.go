75428210965402f64b00aeda02f9d196b21fc608
William Kennedy <bill@ardanlabs.com>
2023-12-13 12:07:08 -0500

    commit 31

--- app/tooling/sales-admin/main.go
+++ app/tooling/sales-admin/main.go
@@ -119,8 +119,28 @@ func GenToken() error {
 	fmt.Printf("%#v\n", claims2)
 	fmt.Println(token2.Signature)
 
+	// -------------------------------------------------------------------------
+	// Authentication in Rego
+
 	return nil
 }
+
+var authentication = `package ardan.rego
+
+import future.keywords.if
+import future.keywords.in
+
+default auth := false
+
+auth if {
+	[valid, _, _] := verify_jwt
+	valid = true
+}
+
+verify_jwt := io.jwt.decode_verify(input.Token, {
+	"cert": input.Key,
+	"iss": input.ISS,
+})`
 
 // GenKey creates an x509 private/public key for auth tokens.
 func GenKey() error {
